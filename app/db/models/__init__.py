1016142953361cc285d84808ebb7b7ac2f45259c
Ivan Omonte <ivanalex.oc@gmail.com>
2026-05-23 14:44:02 -0400

    export models

---
+++ app/db/models/__init__.py
@@ -0,0 +1,5 @@
+from app.db.models.team import Team
+from app.db.models.match import Match
+from app.db.models.predictions_cache import PredictionsCache
+
+__all__ ['Team', 'Match', 'PredictionsCache']
